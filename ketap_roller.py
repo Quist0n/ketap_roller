4e44ed7ed89688327504d717dba55adfd57852d3
Quist0n <37319086+Quist0n@users.noreply.github.com>
2021-05-10 18:59:26 -0500

    Adding named capturing groups to some of the parsing regexps, this prevents having to remove unwanted characters later. Changed the output text to not have a +0 if the modifier is 0. Changed the error text if the user inputs invalid die specifications.

--- ketap_roller.py
+++ ketap_roller.py
@@ -13,11 +13,10 @@ args = sys.argv[1:]
 def roll_dice(dice_num=MIN_DICE, dice_sides=20, mod=DEFAULT_MOD):
     if (dice_num < MIN_DICE) or (dice_sides < MIN_SIDES):
        size_err_msg = f'''
-       dice_num must be
-       {MIN_DICE} or greater and dice_sides must be
-       {MIN_SIDES} or greater, mod must be 0 or greater'''
-       print(size_err_msg)
-       raise ValueError
+       The number of dice be at least
+       {MIN_DICE} or greater and the number of sides must be
+       {MIN_SIDES} or greater'''
+       raise ValueError(size_err_msg)
     else:
         i = 0
         while i < dice_num:
@@ -31,13 +30,11 @@ def parse_args(input_arr):
     while i < len(input_arr):
         try:
 
-            dice_num_regex = r'^\d+d'
-            input_dice_num = re.search(dice_num_regex, input_arr[i]).group()
-            input_dice_num = input_dice_num.replace("d", "")
+            dice_num_regex = r'^(?P<die_num>\d+)d'
+            input_dice_num = re.search(dice_num_regex, input_arr[i]).group('die_num')
 
-            dice_side_regex = r'd\d+\s*'
-            input_dice_sides = re.search(dice_side_regex, input_arr[i]).group()
-            input_dice_sides = input_dice_sides.replace("d","")
+            dice_side_regex = r'd(?P<die_sides>\d+)\s*'
+            input_dice_sides = re.search(dice_side_regex, input_arr[i]).group('die_sides')
 
             dice_mod_pos_regex = r'\+\d+$'
             dice_mod_neg_regex = r'-\d+$'
@@ -47,7 +44,7 @@ def parse_args(input_arr):
 
             if pos_match != None:
 
-                pos_match = pos_match.group().replace("+", "")
+                pos_match = pos_match.group()
                 input_dice_mod = pos_match
 
 
@@ -61,7 +58,7 @@ def parse_args(input_arr):
             input_dice_num = int(input_dice_num)
             input_dice_sides = int(input_dice_sides)
             input_dice_mod = int(input_dice_mod)
-
+            print(input_dice_mod)
         except ValueError or AttributeError:
             print ("Failed to parse dice info")
             sys.exit(1)
@@ -73,9 +70,11 @@ def play(input):
 
     for i in parse_args(input):
         for j,k in roll_dice(i[0], i[1], i[2]):
-            if i[2] >= 0:
+            if i[2] == 0:
+                print(f"Result of {i[0]}d{i[1]} is: {k} [nat: {j}]")
+            elif i[2] > 0:
                 print(f"Result of {i[0]}d{i[1]}+{i[2]} is: {k} [nat: {j}]")
-            elif i[2] < 0:
+            else:
                 print(f"Result of {i[0]}d{i[1]}{i[2]} is: {k} [nat: {j}]")
 
 play(args)
